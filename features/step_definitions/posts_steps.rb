e96dd316337bb46197f9f9019e2edb8ebce0da57
okaruk <karuk.oleh@gmail.com>
2018-05-08 16:49:20 +0300

    Renamed posts step definition file

---
+++ features/step_definitions/posts_steps.rb
@@ -0,0 +1,17 @@
+# Scenario 1
+
+Given(/^I request jsonplaceholder (.*)$/) do |resource|
+    http = Net::HTTP.new("jsonplaceholder.typicode.com", "80")
+    get_url = Net::HTTP::Get.new(resource)
+    @response = http.request(get_url)
+    binding.pry
+end
+
+Given(/^I expect response to have id (\d+)$/) do |id|
+  expect(@response.body).to have_content id
+end
+
+Then(/^Post with id (\d+) has title (.*)$/) do |id, title|
+  expect(@response.body).to have_content title
+end
+
